1d3f427aa93b87c6c78f67f15543b6c7ce949c94
JoShMiQueL <joshmiquel.cs@gmail.com>
2021-08-02 15:56:43 +0200

    Init

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Router from "../src/pages/Home";
+import "../styles/main.scss";
+
+ReactDOM.render(<Router />, document.getElementById("root"));
